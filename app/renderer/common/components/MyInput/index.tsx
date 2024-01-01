892f894450b18263299e0db864296d306a1fa5c3
hassan <hassanbox@qq.com>
2024-01-01 16:52:54 +0800

    MyInput组件

--- app/renderer/common/components/MyInput/index.tsx
+++ app/renderer/common/components/MyInput/index.tsx
@@ -46,7 +46,7 @@ export interface InputProps extends Omit<React.InputHTMLAttributes<HTMLInputElem
     /**
     * @description textarea行数，默认3
     */
-    row?:number;
+    rows?:number;
     /**
     * @description 动态样式
     */
@@ -100,4 +100,150 @@ export default class MyInput extends React.PureComponent<InputProps,InputState>{
             });
         }
     }
+
+    UNSAFE_componentWillReceiveProps(nextProps: Readonly<InputProps>, nextContext: any): void {
+        if(nextProps.value){
+            this.setState({
+                text: nextProps.value,
+            })
+        }
+    }
+
+    saveInput = (input: HTMLInputElement | HTMLTextAreaElement) => {
+        this.input = input;
+    };
+
+    // 模拟change事件
+    actionChange(e: any) {
+        const target = this.input as any;
+        const event = Object.create(e);
+        // 如果是点击清除按钮，则需要改target指向input，value清空
+        if(e.type === 'click'){
+            target.value = '';
+            event.target = target;
+            event.CurrentTarget = target;
+        }
+        this.props.onChange && this.props.onChange(event);
+    }
+
+    focus = () => {
+        this.input && this.input.focus();
+    };
+
+    blur = () => {
+        this.input && this.input.blur();
+    };
+
+    onFocus = () => {
+        this.setState({
+            focus: false,
+        });
+    };
+
+    onBlur = () => {
+        this.setState({
+          focus: false,
+        });
+    };
+
+    onInput = (e: any) => {
+        this.setState({text: e.target.value});
+        this.actionChange(e);
+    };
+
+    onClear = (e: any) => {
+        this.setState({text: ''});
+        this.actionChange(e);
+    };
+
+    renderBefore(){
+        const { addonBefore } = this.props;
+        return !!addonBefore && <div styleName="my-input-center">{addonBefore}</div>;
+    }
+
+    renderAfter(){
+        const {addonAfter} = this.props;
+        return !!addonAfter && <div styleName="my-input-center">{addonAfter}</div>;
+    }
+
+    renderClear() {
+        const {allowClear} = this.props;
+        return !!allowClear && this.state.text && <i styleName="my-input-clear" onClick={this.onClear} />;
+    }
+
+    renderInput() {
+        const { placeholder, size = 'normal', maxLength, id, disabled, autoFocus } = this.props;
+        return (
+            <div
+                styleName={classNames(`my-input-input`, {
+                [`${size}`]: true,
+                })}
+            >
+                <input
+                {...{ placeholder, maxLength, id, disabled, autoFocus }}
+                value={this.state.text}
+                onFocus={this.onFocus}
+                onBlur={this.onBlur}
+                onInput={this.onInput}
+                ref={this.saveInput as any}
+                />
+                {this.renderClear()}
+            </div>
+        );
+    }
+
+    renderTextarea(){
+        const { placeholder, maxLength = 1000, id, disabled, allowCount = true, autoFocus, rows } = this.props;
+        const _rows = rows || 3;
+        const text = this.state.text;
+        return (
+            <div styleName="my-input-textarea" style={{ height: 24 * _rows }}>
+              <textarea
+                {...{ placeholder, maxLength, id, disabled, autoFocus }}
+                rows={_rows}
+                value={text}
+                onFocus={this.onFocus}
+                onBlur={this.onBlur}
+                onInput={this.onInput}
+                ref={this.saveInput as any}
+              />
+              {this.renderClear()}
+              {allowCount && (
+                <div styleName="my-input-textarea-footer">
+                  <span
+                    styleName={classNames({
+                      'max-length-text': !!maxLength && text && String(text).length >= maxLength,
+                    })}
+                  >
+                    {String(text).length}
+                  </span>
+                  {!!maxLength && (
+                    <>
+                      <span>/</span>
+                      <span>{maxLength}</span>
+                    </>
+                  )}
+                </div>
+              )}
+            </div>
+        );
+    }
+
+    render() {
+        const { bgTransparent = false, style, type, allowClear } = this.props;
+        return (
+          <div
+            style={style}
+            styleName={classNames('my-input', {
+              normal: !bgTransparent,
+              focus: this.state.focus,
+              'allow-clear': allowClear,
+            })}
+          >
+            {this.renderBefore()}
+            {TYPE.textarea === type ? this.renderTextarea() : this.renderInput()}
+            {this.renderAfter()}
+          </div>
+        );
+      }
 }
